cd4e6d90ce8b0def6d7a1ca064ea4d5535354453
DPU J S <DPU J S@DESKTOP-12TU7QI>
2020-10-07 21:22:19 +0530

    Add project files.

---
+++ IEnumerableInterfaces/OddGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IEnumerableInterfaces
+{
+   public  class OddGenerator: IEnumerable<int>
+    {
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            int i = 1;
+            yield return i;
+
+            while (true)
+            {
+                i += 2;
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ IEnumerableInterfaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace IEnumerableInterfaces
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Odd Numbers: ");
+
+            var generartor = new OddGenerator();
+            foreach (var odd in generartor)
+            {
+                if (odd > 50) break;
+                Console.WriteLine(odd);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
